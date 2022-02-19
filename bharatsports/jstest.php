44b09093e5c224de66fe2efacb6cca5132e46c0e
Mahendra Gupta <65041909+MahendraGupta91@users.noreply.github.com>
2022-02-19 22:36:52 +0530

    commit master

---
+++ bharatsports/jstest.php
@@ -0,0 +1,19 @@
+<html>
+
+   <head>
+      <title>jQuery Function</title>
+      <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+      <script>
+        $(document).ready(function() {
+         
+            alert("Hello, world!");
+          
+        });
+      </script>
+   </head>
+   
+   <body>
+      <span class="like-btn"></span>
+   </body>
+</html>
---
+++ bharatsports/like.php
@@ -0,0 +1,46 @@
+<?php
+            $con = mysqli_connect("localhost","root","") or die("Unable to connect");
+                   mysqli_select_db($con,"bharatsports");
+                   session_start();
+
+                $product_id=$_POST['like_pro_id'];
+                $userid=$_POST['userid'];
+                $like="like-btn is-active";
+               
+
+                $sql = "SELECT FROM `likes` WHERE userid='$userid' AND like_pro_id='$product_id'";
+                $result =mysqli_query($con, $sql);
+                if (mysqli_num_rows($result)>0) 
+                {
+                    $sql = "DELETE FROM `likes` WHERE userid='$userid' AND like_pro_id='$product_id'";
+                    $result =mysqli_query($con, $sql);
+                    if (mysqli_num_rows($result)>0) 
+                    {
+                    echo json_encode(array("statusCode"=>200));
+                    }
+                    else 
+                    {
+                       echo json_encode(array("statusCode"=>201));
+                    }
+                }
+                else
+                {
+                    
+                    $sql = "INSERT INTO `likes`( `userid`, `like_pro_id`, `like_status`) 
+                    VALUES ('$userid','$product_id','$like')";
+
+                    if (mysqli_query($con, $sql)) 
+                    {
+                      echo json_encode(array("statusCode"=>200));
+                    } 
+                     else 
+                    {
+                       echo json_encode(array("statusCode"=>201));
+                    }
+                }
+
+                
+            
+
+                mysqli_close($con);  
+?>
